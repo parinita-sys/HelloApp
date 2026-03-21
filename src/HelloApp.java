27d715e1a4dc70e6648a56b89663c7506d1e76ac
parinita-sys <ps0829@srmist.edu.in>
2026-03-22 04:11:06 +0530

    Coded UC3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,7 +1,8 @@
 public class HelloApp {
     public static void main(String[] args) {
+        String name = "World";
         if (args.length > 0) {
-            String name = args[0];
+             name = args[0];
             System.out.println("Hello, " + name + "!");
         } else {
             System.out.println("Hello, stranger!");
