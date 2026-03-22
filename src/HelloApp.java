551db0d04bd344b803d41ef74f5932a0137c8ba1
parinita-sys <ps0829@srmist.edu.in>
2026-03-22 19:41:47 +0530

    Coded UC5

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,15 +2,17 @@ class HelloApp {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
-        } else {
-            System.out.print("Hello, ");
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-                if (i < args.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println("!");
+            return;
         }
+        System.out.print("Hello, ");
+        int count = 0;
+        for (String name : args) {
+            System.out.print(name);
+            count++;
+        if (count < args.length) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("!");
     }
 }
